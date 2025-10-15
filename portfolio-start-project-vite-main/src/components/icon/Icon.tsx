c6b641a0e425195076444f17c7e16f13fe90fbc5
Viktoriya Gromova <rockingirl@mail.ru>
2025-10-15 16:19:32 +0500

    styled Footer section

--- portfolio-start-project-vite-main/src/components/icon/Icon.tsx
+++ portfolio-start-project-vite-main/src/components/icon/Icon.tsx
@@ -1,4 +1,5 @@
 import iconsSprite from '../../assets/images/svg-sprite.svg'
+import styled from "styled-components";
 
 type IconPropsType = {
     iconId: string,
@@ -9,9 +10,13 @@ type IconPropsType = {
 
 export const Icon = (props: IconPropsType) => {
     return (
-        <svg width={props.width || "50"} height={props.height || "50"} viewBox={props.viewBox || "0 0 50 50"}
+        <StyledSvg width={props.width || "50"} height={props.height || "50"} viewBox={props.viewBox || "0 0 50 50"}
              fill="none" xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
-        </svg>
+        </StyledSvg>
     );
 };
+
+const StyledSvg = styled.svg`
+
+`
--- portfolio-start-project-vite-main/src/layout/footer/Footer.tsx
+++ portfolio-start-project-vite-main/src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {Container} from "../../components/Container.tsx";
+import {theme} from "../../styles/Theme.ts";
 
 export const Footer = () => {
     return (
@@ -11,22 +12,22 @@ export const Footer = () => {
                     <Name>Svetlana</Name>
                     <SocialList>
                         <SocialItem>
-                            <SocialLink>
+                            <SocialLink href={""}>
                                 <Icon iconId={"instagram"} width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"}/>
                             </SocialLink>
                         </SocialItem>
                         <SocialItem>
-                            <SocialLink>
+                            <SocialLink href={""}>
                                 <Icon iconId={"telegram"} width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"}/>
                             </SocialLink>
                         </SocialItem>
                         <SocialItem>
-                            <SocialLink>
+                            <SocialLink href={""}>
                                 <Icon iconId={"vk"} width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"}/>
                             </SocialLink>
                         </SocialItem>
                         <SocialItem>
-                            <SocialLink>
+                            <SocialLink href={""}>
                                 <Icon iconId={"linkedIn"} width={"21px"} height={"21px"} viewBox={"0 0 21px 21px"}/>
                             </SocialLink>
                         </SocialItem>
@@ -39,23 +40,45 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-    background-color: #ffac6b;
-    min-height: 20vh;
+    background-color: ${theme.colors.primaryBG};
+    padding: 40px 0;
+
 `
 const Name = styled.span`
-
+    font-family: "Josefin Sans", sans-serif;
+    font-weight: 700;
+    font-size: 22px;
+    letter-spacing: 3px;
 `
 const SocialList = styled.ul`
     display: flex;
-    gap: 30px;
+    gap: 20px;
+    margin: 30px 0;
 `
 const SocialItem = styled.li`
 
 `
 
 const SocialLink = styled.a`
+    width: 35px;
+    height: 35px;
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 50%;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    color: ${theme.colors.accent};
+    &:hover {
+        color: ${theme.colors.primaryBG};
+        background-color: ${theme.colors.accent};
+        transform: translateY(-4px);
+    }
 `
 const Copyright = styled.small`
-
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    opacity: 0.5;
 `
