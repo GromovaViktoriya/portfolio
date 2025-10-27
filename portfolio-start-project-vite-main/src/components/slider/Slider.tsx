5f5eac2dc683d5492368faf36ec6989799882271
Viktoriya Gromova <rockingirl@mail.ru>
2025-10-27 17:53:44 +0500

    refactoring slider

--- portfolio-start-project-vite-main/src/components/slider/Slider.tsx
+++ portfolio-start-project-vite-main/src/components/slider/Slider.tsx
@@ -1,72 +1,24 @@
-import styled from "styled-components";
 import {FlexWrapper} from "../FlexWrapper.tsx";
-import {theme} from "../../styles/Theme.ts";
+import { S } from "./Slider_Styles.ts";
+import React from "react";
 
-export const Slider = () => {
+export const Slider: React.FC = () => {
     return (
-        <StyledSlider>
+        <S.Slider>
             <FlexWrapper>
-                <Slide>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                <S.Slide>
+                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit.</Text>
-                    <Name>@ivan ivanow</Name>
-                </Slide> 
+                        elit.</S.Text>
+                    <S.Name>@ivan ivanow</S.Name>
+                </S.Slide>
             </FlexWrapper>
-            <Pagination>
+            <S.Pagination>
                 <span></span>
                 <span className={"active"}></span>
                 <span></span>
-            </Pagination>
+            </S.Pagination>
 
-        </StyledSlider>
+        </S.Slider>
     );
 };
-
-const StyledSlider = styled.div`
-    border: 1px solid red;
-    max-width: 500px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-
-const Slide = styled.div`
-  
-    text-align: center;
-`
-
-const Text = styled.p`
-
-`
-
-const Name = styled.span`
-    font-family: "Josefin Sans", sans-serif;
-    font-weight: 600;
-    font-size: 16px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    display: inline-block;
-    margin: 22px 0 32px;
-`
-
-const Pagination = styled.div`
-
-    span {
-        display: inline-block;
-        width: 7px;
-        height: 7px;
-        background: rgba(255, 255, 255, 0.5);
-        border-radius: 50%;
-
-        & + span {
-            margin-left: 5px;
-        }
-        
-        &.active {
-            background: ${theme.colors.accent};
-            width: 20px;
-            border-radius: 20px;
-        }
-    }
-`
---
+++ portfolio-start-project-vite-main/src/components/slider/Slider_Styles.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import {theme} from "../../styles/Theme.ts";
+
+//slider
+const Slider = styled.div`
+    border: 1px solid red;
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Slide = styled.div`
+  
+    text-align: center;
+`
+
+const Text = styled.p`
+
+`
+
+const Name = styled.span`
+    font-family: "Josefin Sans", sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    display: inline-block;
+    margin: 22px 0 32px;
+`
+
+const Pagination = styled.div`
+
+    span {
+        display: inline-block;
+        width: 7px;
+        height: 7px;
+        background: rgba(255, 255, 255, 0.5);
+        border-radius: 50%;
+
+        & + span {
+            margin-left: 5px;
+        }
+        
+        &.active {
+            background: ${theme.colors.accent};
+            width: 20px;
+            border-radius: 20px;
+        }
+    }
+`
+
+
+export const S = {
+    Slider,
+    Slide,
+    Text,
+    Name,
+    Pagination,
+}
