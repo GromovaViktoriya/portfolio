974709ba880abbb085b39de67769b1de3da513ca
Viktoriya Gromova <rockingirl@mail.ru>
2025-10-30 19:30:13 +0500

    Typewriter-effect for h1 in Main

--- portfolio-start-project-vite-main/src/layout/sections/main/Main.tsx
+++ portfolio-start-project-vite-main/src/layout/sections/main/Main.tsx
@@ -2,9 +2,11 @@ import photo from '../../../assets/images/photo.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper.ts";
 import {Container} from "../../../components/Container.ts";
 import React from "react";
-import { S } from './Main_Styles.ts';
+import {S} from './Main_Styles.ts';
+import Typewriter from 'typewriter-effect';
 
-export const Main:React.FC = () => {
+
+export const Main: React.FC = () => {
     return (
         <S.Main>
             <Container>
@@ -12,7 +14,16 @@ export const Main:React.FC = () => {
                     <S.TextWrapper>
                         <S.SmallText>Hi there</S.SmallText>
                         <S.Name>I am <span>Svetlana Dyablo</span></S.Name>
-                        <S.MainTitle>A Web Developer.</S.MainTitle>
+                        <S.MainTitle>
+                            <p>A Web Developer.</p>
+                            <Typewriter
+                            options={{
+                                strings: ['A Web Developer.', 'A Frontend Developer.', 'A Web Designer.'],
+                                autoStart: true,
+                                loop: true,
+                                delay: 150
+                            }}
+                        /></S.MainTitle>
                     </S.TextWrapper>
                     <S.PhotoWrapper>
                         <S.Photo src={photo} alt=""/>
