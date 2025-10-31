99115b0294e4aa502e6af0a6d1c3e826475ba0f9
Viktoriya Gromova <rockingirl@mail.ru>
2025-10-31 17:15:56 +0500

    added emailJs server for sending emails

--- portfolio-start-project-vite-main/src/layout/sections/contacts/Contact.tsx
+++ portfolio-start-project-vite-main/src/layout/sections/contacts/Contact.tsx
@@ -2,18 +2,43 @@ import {SectionTitle} from "../../../components/SectionTitle.ts";
 import {Button} from "../../../components/Button.ts";
 import {Container} from "../../../components/Container.ts";
 import { S } from "./Contacts_Styles.ts";
-import React from "react";
+import React, {ElementRef, useRef} from "react";
+import emailjs from '@emailjs/browser';
 
 export const Contact : React.FC = () => {
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(!form.current){
+            return
+        }
+
+        emailjs
+            .sendForm('service_gqjr6kq', 'template_jdxhbvj', form.current, {
+                publicKey: 'mo4SZjuzb3I75K9ON',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                });
+        e.currentTarget.reset();
+    };
+
     return (
         <S.Contact id={"contact"}>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <S.Form>
-                    <S.Field placeholder={"Name"}/>
-                    <S.Field placeholder={"Subject"}/>
-                    <S.Field as={"textarea"} placeholder={"Message"}/>
-                    <Button>Send message</Button>
+                <S.Form ref={form} onSubmit={sendEmail}>
+                    <S.Field required placeholder={"Name"} name={"user_name"}/>
+                    <S.Field required placeholder={"Email"} name={"email"}/>
+                    <S.Field required placeholder={"Subject"} name={"subject"}/>
+                    <S.Field required as={"textarea"} placeholder={"Message"} name={"message"}/>
+                    <Button type="submit">Send message</Button>
                 </S.Form>
             </Container>
         </S.Contact>
