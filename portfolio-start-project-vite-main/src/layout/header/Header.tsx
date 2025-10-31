91fe7e48ce402dc9d06dffb1f18b970285595d3f
Viktoriya Gromova <rockingirl@mail.ru>
2025-10-31 13:01:26 +0500

    refactored menu items

--- portfolio-start-project-vite-main/src/layout/header/Header.tsx
+++ portfolio-start-project-vite-main/src/layout/header/Header.tsx
@@ -6,8 +6,6 @@ import {MobileMenu} from "./headerMenu/mobileMenu/MobileMenu.tsx";
 import React from "react";
 import {S} from "./Header_Styles.ts";
 
-const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
-
 export const Header : React.FC = () => {
     const [width, setWidth] = React.useState(window.innerWidth);
     const breakpoint = 768;
@@ -23,8 +21,8 @@ export const Header : React.FC = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
-                    {width < breakpoint ? <MobileMenu menuItems={items}/>
-                                        : <DesktopMenu menuItems={items}/>}
+                    {width < breakpoint ? <MobileMenu/>
+                                        : <DesktopMenu/>}
                 </FlexWrapper>
             </Container>
         </S.Header>
