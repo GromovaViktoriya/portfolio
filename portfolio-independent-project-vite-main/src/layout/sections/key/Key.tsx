2522efc2663719d9d4a553ac05e300b32de07c3b
Viktoriya Gromova <rockingirl@mail.ru>
2025-11-03 20:23:39 +0500

    added awesome-reveal library to create fade effect for skills

--- portfolio-independent-project-vite-main/src/layout/sections/key/Key.tsx
+++ portfolio-independent-project-vite-main/src/layout/sections/key/Key.tsx
@@ -4,6 +4,7 @@ import {Skill} from "./skill/Skill.tsx";
 import {Container} from "../../../components/Container.tsx";
 import {About} from "./about/About.tsx";
 import {theme} from "../../../styles/Theme.ts";
+import { Fade } from "react-awesome-reveal";
 
 
 export const Key = () => {
@@ -11,17 +12,19 @@ export const Key = () => {
         <StyledKey>
             <Container>
                 <StyledSkills>
-                    <FlexWrapper wrap={"wrap"} gap={"30px"} justify={"center"}>
-                        <Skill iconId={"product-design"} width={"100px"} height={"80px"} viewBox={"0 0 100 80"}
-                               text={"Product Design"}/>
-                        <Skill iconId={"ux-ui-design"} width={"143px"} height={"48px"} viewBox={"0 0 143 48"}
-                               text={"UX/UI Design"} marginTop={"27px"}/>
-                        <Skill iconId={"icon-design"} text={"Icon Design"}/>
-                        <Skill iconId={"logo-design"} text={"Logo Design"}/>
-                        <Skill iconId={"backend"} text={"Backend"}/>
-                        <Skill iconId={"frontend"} text={"Frontend"}/>
-                        <Skill iconId={"motion"} text={"Motion"}/>
-                    </FlexWrapper>
+                    <Fade cascade={true}>
+                        <FlexWrapper wrap={"wrap"} gap={"30px"} justify={"center"}>
+                            <Skill iconId={"product-design"} width={"100px"} height={"80px"} viewBox={"0 0 100 80"}
+                                   text={"Product Design"}/>
+                            <Skill iconId={"ux-ui-design"} width={"143px"} height={"48px"} viewBox={"0 0 143 48"}
+                                   text={"UX/UI Design"} marginTop={"27px"}/>
+                            <Skill iconId={"icon-design"} text={"Icon Design"}/>
+                            <Skill iconId={"logo-design"} text={"Logo Design"}/>
+                            <Skill iconId={"backend"} text={"Backend"}/>
+                            <Skill iconId={"frontend"} text={"Frontend"}/>
+                            <Skill iconId={"motion"} text={"Motion"}/>
+                        </FlexWrapper>
+                    </Fade>
                 </StyledSkills>
                 <About/>
             </Container>
